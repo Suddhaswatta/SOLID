95128cbd889d13de0a997b29a88de089b78b705c
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2021-11-13 19:58:31 +0530

    Added DOB

--- Builder/person.py
+++ Builder/person.py
@@ -3,12 +3,13 @@ from abc import ABC, abstractmethod
 
 class Person:
     def __init__(self):
-        self.name = None
+        self.first_name = None
+        self.last_name = None
         self.position = None
         self.date_of_birth = None
         
     def __str__(self) :
-        return f'Person : {self.name}, works as : {self.position}\nwas born in {self.date_of_birth} '
+        return f'Born in {self.date_of_birth} ,{self.first_name} {self.last_name}, works as : {self.position}'
     
     @staticmethod
     def create():
@@ -26,19 +27,32 @@ class PersonBuilder:
         self.person.position = job.upper()
         return self
     
-    def known_as(self,name):
-        self.person.name = name.upper()
+    def known_as_with_firstname(self,first_name):
+        self.person.first_name = first_name.capitalize()
         return self
-   
+    
+    def known_as_with_last_name(self,last_name):
+        self.person.last_name = last_name
+        return self
+    
+    def born_in(self,dob):
+        self.person.date_of_birth = dob
+        return self
+    
 personA = Person.create()\
-            .known_as("Eve")\
-                .works_as("Scrum Master")\
-                    .build()
-                    
+            .known_as_with_firstname("Souraj")\
+                .known_as_with_last_name('Maity')\
+                    .works_as("Scrum Master")\
+                        .born_in("2 Feb 1996")\
+                            .build()
+
 personB = Person.create()\
-            .known_as(name="Adam")\
-                .works_as(job="Developer")\
-                    .build()        
+            .known_as_with_firstname("William")\
+                .known_as_with_last_name('Wesker')\
+                    .works_as("Scientist")\
+                        .born_in("Unknown")\
+                            .build()
+                    
 print(personA)
 print(personB)
     
